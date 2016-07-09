e4578ef2df1c4fb333329ecb2524d9b6fc32ad03
Virginia Van Keuren <virginia.vankeuren@gmail.com>
2016-07-09 19:51:56 -0400

    readded jQuery and added animate.css

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.artist-list').hide();
+
+  $('.searchfld').on('keypress', function(){
+     event.stopPropagation();
+    $(this).closest('.col-xs-6').find('.artist-list').slideToggle();
+  })
+});
