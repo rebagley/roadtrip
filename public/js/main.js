ae59991a4f783d0b3c8ab96b966faa1d285eeb06
Ruth <rebagley@haverford.edu>
2016-07-09 20:40:20 -0400

    moving things around

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
+/*fun jQuery stuff in here*/
 $(document).ready(function(){
   $('.artist-list').hide();
 
   $('.searchfld').on('keypress', function(){
-     event.stopPropagation();
+  //  event.stopPropagation();
     $(this).closest('.col-xs-6').find('.artist-list').slideToggle();
   })
 });
