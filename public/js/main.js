16073fd7a5021db574791cde369534f19097392b
Virginia Van Keuren <virginia.vankeuren@gmail.com>
2016-07-09 21:56:26 -0400

    added two menu for mobile on accident

--- public/js/main.js
+++ public/js/main.js
@@ -10,4 +10,29 @@ $(document).ready(function(){
   $('.heading').addClass('animated bounceIn');
   // $('.shittyLogo').animate({opacity: 0}, 1000);
   // $('.shittyLogo:hidden:first').animate({opacity: 1}, 1000);
+
+  $(function() {
+      $('#navigation-bar ul').hide();
+      var pull = $('#pull');
+          menu = $('#navigation-bar ul');
+          menuHeight  = menu.height();
+      $(pull).on('click', function(e) {
+          e.preventDefault();
+          menu.slideToggle();
+      });
+  });
+
+  var $window = $(window),
+      $nav = $('#navigation-bar'),
+      $button = $('#pull');
+
+      $button.on('click', function (){
+          $nav.slideToggle();
+      });
+
+      $window.on('resize', function (){
+          if ($window.width() > 400){
+              $nav.show();
+          }
+      });
 });
