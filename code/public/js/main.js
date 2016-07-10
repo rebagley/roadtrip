e5116bff806322aa898593782e5aee8725f39436
Virginia Van Keuren <virginia.vankeuren@gmail.com>
2016-07-09 23:30:14 -0400

    deleted extra jQuery folder

--- code/public/js/main.js
+++
@@ -1,9 +0,0 @@
-/*fun jQuery stuff in here*/
-$(document).ready(function(){
-  $('.artist-list').hide();
-
-  $('.searchfld').on('keypress', function(){
-  //  event.stopPropagation();
-    $(this).closest('.col-xs-6').find('.artist-list').slideToggle();
-  })
-});
