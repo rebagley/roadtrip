5d1d68b06bf76d33bcf7f4a28e65837aa52a3dbe
Ruth <rebagley@haverford.edu>
2016-07-10 04:45:23 -0400

    dfeafa

--- public/js/main.js
+++ public/js/main.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
   $('.searchfld').on('keypress', function(e){
   //  event.stopPropagation();
     $(this).closest('.col-xs-6').find('.artist-list').slideToggle();
-
-    if(e.keyCode == 13){
-      $('#findMatches').trigger('click')
-    }
+    //
+    // if(e.keyCode == 13){
+    //   $('#findMatches').trigger('click')
+    // }
 
   })
 
@@ -17,13 +17,13 @@ $(document).ready(function(){
   // $('.shittyLogo').addClass('animated bounceIn');
   // $('.shittyLogo').animate({opacity: 0}, 1000);
   // $('.shittyLogo:hidden:first').animate({opacity: 1}, 1000);
-
-  $('#findMatches').click(function(){
-    var artist1 = $('#searchfld1').attr('value');
-    var artist2 = $('#searchfld2').attr('value');
-    console.log('ARTISTS:'+ artist1,artist2)
-    window.location.href = '/search?artist1='+encodeURIComponent(artist1)+"&artist2="+encodeURIComponent(artist2);
-  })
+  //
+  // $('#findMatches').click(function(){
+  //   var artist1 = $('#searchfld1').attr('value');
+  //   var artist2 = $('#searchfld2').attr('value');
+  //   console.log('ARTISTS:'+ artist1,artist2)
+  //   window.location.href = '/search?artist1='+encodeURIComponent(artist1)+"&artist2="+encodeURIComponent(artist2);
+  // })
 
   $(function() {
       var pull = $('#pull');
