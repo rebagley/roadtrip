7dcbd37c7760d7d917ced7fcd406b73abd77c5c0
Virginia Van Keuren <virginia.vankeuren@gmail.com>
2016-07-09 20:02:42 -0400

    added animation to heading

--- public/js/main.js
+++ public/js/main.js
@@ -6,4 +6,8 @@ $(document).ready(function(){
   //  event.stopPropagation();
     $(this).closest('.col-xs-6').find('.artist-list').slideToggle();
   })
+
+  $('.heading').addClass('animated bounceIn');
+  // $('.shittyLogo').animate({opacity: 0}, 1000);
+  // $('.shittyLogo:hidden:first').animate({opacity: 1}, 1000);
 });
